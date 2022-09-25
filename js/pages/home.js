7f8eb91f3aa4e64ef1a1fae1f4db12855f7f9362
Mindaugas Krivickas <mkrivickas@gmail.com>
2022-09-25 20:10:32 +0300

    Layout css and git conflict prevention

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,45 @@
 // component imports
 
 // component execution
+
+/* header: start  */
+/* header: end  */
+
+/* hero: start  */
+/* hero: end  */
+
+/* premium features: start  */
+/* premium features: end  */
+
+/* communication gap: start  */
+/* communication gap: end  */
+
+/* device manage: start  */
+/* device manage: end  */
+
+/* how app works: start  */
+/* how app works: end  */
+
+/* interface slide: start  */
+/* interface slide: end  */
+
+/* testimonials: start  */
+/* testimonials: end  */
+
+/* FAQ: start  */
+/* FAQ: end  */
+
+/* team: start  */
+/* team: end  */
+
+/* app store: start  */
+/* app store: end  */
+
+/* subscribe: start  */
+/* subscribe: end  */
+
+/* contacts: start  */
+/* contacts: end  */
+
+/* footer: start  */
+/* footer: end  */
